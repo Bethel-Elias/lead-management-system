cf990ce7d834acb53e17b07c7266781501915ee5
bethe-elias <betineba1@gmail.com>
2026-07-27 00:59:11 +0300

    server.js updated

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 sequelize
   .authenticate()
   .then(() => {
-    console.log("MySQL Connected");
+    console.log("PostgreSQL Connected");
 
     return sequelize.sync({ alter: true });
   })
